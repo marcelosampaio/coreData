2eee51c22eb24fa93a90501c79ff155e3e90c2e6
Marcelo Sampaio <marcelo@artlogica.com.br>
2014-09-12 12:36:31 -0300

    Initial Commit

---
+++ coreData/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  coreData
+//
+//  Created by Marcelo Sampaio on 9/12/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
